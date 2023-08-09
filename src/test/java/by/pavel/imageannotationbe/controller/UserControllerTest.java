0b780a7d7ce4c06aced25a8b9bd2c6d20564bf53
PaulTheStrong <98765432123456789r@gmail.com>
2023-08-09 16:54:20 +0300

    More tests for user controller

--- src/test/java/by/pavel/imageannotationbe/controller/UserControllerTest.java
+++ src/test/java/by/pavel/imageannotationbe/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package by.pavel.imageannotationbe.controller;
 
 import by.pavel.imageannotationbe.dto.CredentialsDto;
 import by.pavel.imageannotationbe.dto.RegistrationDto;
+import by.pavel.imageannotationbe.dto.UserDto;
 import by.pavel.imageannotationbe.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
 
     private static final Map<String, String> ACCESS_TOKEN = Map.of("access_token", "token");
+    private static final UserDto USER_DTO = new UserDto(1L, "email", "name", "surname", "passwordHash", List.of());
 
     @Autowired
     private MockMvc mockMvc;
@@ -61,5 +66,30 @@ class UserControllerTest {
                 .andExpect(jsonPath("$").doesNotExist());
 
         verify(userService).register(registrationDto);
+    }
+
+    @Test
+    void getMe() throws Exception {
+        //given
+        when(userService.getMe()).thenReturn(USER_DTO);
+        //when
+        mockMvc.perform(get("/me"))
+        //then
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(USER_DTO)));
+    }
+
+    @Test
+    void updateMe() throws Exception {
+        when(userService.updateMe(USER_DTO)).thenReturn(USER_DTO);
+        //when
+        mockMvc.perform(post("/me")
+                        .content(objectMapper.writeValueAsString(USER_DTO))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(USER_DTO)));
+
+        verify(userService).updateMe(USER_DTO);
     }
 }
